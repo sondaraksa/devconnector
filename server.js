193403a19789504ef731c88d1d0e23d10ce2bf73
sondaraksa <son_daraksa@hotmail.com>
2019-01-29 12:36:12 +0700

    update to server

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const passport = require("passport");
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
+const path = require("path");
 
 const app = express();
 
@@ -35,6 +36,14 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+//server static assets if production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
