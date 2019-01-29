69ee72971c7ff892741b4f2addee3c84f4bf2042
sondaraksa <son_daraksa@hotmail.com>
2019-01-29 12:46:52 +0700

    update server

--- server.js
+++ server.js
@@ -40,7 +40,9 @@ app.use("/api/posts", posts);
 //server static assets if production
 if (process.env.NODE_ENV === "production") {
   //set static folder
-  app.use("*", (req, res) => {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
